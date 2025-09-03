394e491119b8df512b8d58e768b95b26a5efdcdf
Marcos <161627757+Marcos021108@users.noreply.github.com>
2025-09-03 14:35:14 -0300

    .........

--- primeiro-teste/src/components/Formulari.js
+++ primeiro-teste/src/components/Formulari.js
@@ -1,16 +1,28 @@
+import {useState} from 'react'
+
 function Formulario(){
 
     function cadastrar(e){
         e.preventDefault()
-        console.log("usuario cadastrado")
+        //console.log(nome)
+       //console.log("usuario cadastrado")
+       console.log(`Usuario ${nome} cadastrado com a senha ${senha}`)
     }
+
+    const [nome, setNome] = useState('Marcos Gabriel')
+    const [senha, setSenha] = useState('')
 
     return(
         <div>
             <h1>Cadastro</h1>
             <form onSubmit={cadastrar}>
                 <div>
-                    <input type="text" placeholder="digite seu nome"/>
+                    <label htmlFor="nome"> Nome:</label>
+                    <input type="text" id="nome" name="nome" placeholder="digite seu nome" value={nome} onChange={(e)=> setNome(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor="senha"> Senha:</label>
+                    <input type="password" id="senha" name="senha" placeholder="digite seu senha" onChange={(e)=> setSenha(e.target.value)}/>
                 </div>
                 <div>
                     <input type="submit" value="cadastrar"/>
